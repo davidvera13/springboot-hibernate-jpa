f343da4440a77749b75ca148b7cd9cf14c15a46f
davidvera13 <david.vera@9online.fr>
2025-09-14 16:40:19 +0200

    feat(hibernate jpa): introduction to testing with data boot impl

--- Hibernate and Spring Data JPA/02-introduction-to-testing-with-springboot/hibernate-jpa/src/test/java/eu/dreamlabs/hibernatejpa/HibernateJpaApplicationTests.java
+++ Hibernate and Spring Data JPA/02-introduction-to-testing-with-springboot/hibernate-jpa/src/test/java/eu/dreamlabs/hibernatejpa/HibernateJpaApplicationTests.java
@@ -1,13 +1,30 @@
 package eu.dreamlabs.hibernatejpa;
 
+import eu.dreamlabs.hibernatejpa.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+// it loads springboot context
 @SpringBootTest
 class HibernateJpaApplicationTests {
+    @Autowired
+    BookRepository bookRepository;
+
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void bookRepositoryCount() {
+        long count = bookRepository.count();
+        System.out.println("Number of books: " + count);
+        // when srarting, books are initialized
+        assertThat(count).isGreaterThan(0);
+        // valid only if we have empty database at startup. Won't work in production mod
+        // assertThat(count).isEqualTo(2);
+    }
 }
---
+++ Hibernate and Spring Data JPA/02-introduction-to-testing-with-springboot/hibernate-jpa/src/test/java/eu/dreamlabs/hibernatejpa/SpringBootJpaSliceTest.java
@@ -0,0 +1,72 @@
+package eu.dreamlabs.hibernatejpa;
+
+import eu.dreamlabs.hibernatejpa.entity.BookEntity;
+import eu.dreamlabs.hibernatejpa.repository.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.annotation.Commit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// load jpa context not all spring boot complete dependencies
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SpringBootJpaSliceTest {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    @Order(1)
+    void testJpaTestSplice() {
+        long countBefore = bookRepository.count();
+        System.out.println("Number of books before: " + countBefore);
+        bookRepository.save(new BookEntity("My Book", "Author2", "1235555", "Self"));
+        long countAfter = bookRepository.count();
+        System.out.println("Number of books after: " + countAfter);
+
+        assertThat(countBefore).isLessThan(countAfter);
+        assertThat(countAfter).isEqualTo(countBefore + 1);
+        // we don't trigger command line runner call
+        assertThat(countBefore).isEqualTo(0);
+    }
+
+    // default behaviour is to roll back the transaction: this test will fail if run alone
+    @Test
+    @Order(2)
+    void testJpaTestSpliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(0);
+    }
+
+    // here transaction will be commited
+    // we can either use @Rollback set to false or @Commit
+    @Test
+    @Order(3)
+    // @Rollback(false)
+    @Commit
+    void testJpaTestSpliceNoRollback() {
+        long countBefore = bookRepository.count();
+        bookRepository.save(new BookEntity("My Book", "Author2", "1235555", "Self"));
+        long countAfter = bookRepository.count();
+
+        // Expected :2L
+        // Actual   :0L
+        // assertThat(countBefore).isEqualTo(2);
+        assertThat(countBefore).isLessThan(countAfter);
+        assertThat(countAfter).isEqualTo(countBefore + 1);
+    }
+
+    // default behaviour is to roll back the transaction ...
+    // this test must be run after testJpaTestSpliceNoRollback
+    @Test
+    @Order(4)
+    void testJpaTestSpliceTransactionNoRollback() {
+        long countBefore = bookRepository.count();
+        // Error won't be triggered this time
+        assertThat(countBefore).isEqualTo(1);
+    }
+}
